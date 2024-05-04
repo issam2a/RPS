cb9f363c2350230e12393154c8c4d2d7c877bf05
issam <issamissa2a@gmail.com>
2024-05-05 01:17:13 +0300

    write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,22 +1,42 @@
-const choiceArray =['rock','paper','scissors'];
-function getComputerChoice(){
-    let randomChoice = Math.floor(Math.random() * choiceArray.length);
-    let computerChoice = choiceArray[randomChoice];
-    return computerChoice;
+const choiceArray = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let randomChoice = Math.floor(Math.random() * choiceArray.length);
+  let computerChoice = choiceArray[randomChoice];
+  return computerChoice;
 }
-getComputerChoice();
+let computer = getComputerChoice();
 
-
-function getHumanChoice(){
-    let humanChoice = prompt('Chose your weapon ');
-    humanChoice = humanChoice.toLowerCase();
-    if(!choiceArray.includes(humanChoice)){
-        alert ('that is not a weapon !!')
-        return getHumanChoice();
-    }
-    return humanChoice;
-
+function getHumanChoice() {
+  let humanChoice = prompt("Chose your weapon ");
+  humanChoice = humanChoice.toLowerCase();
+  if (!choiceArray.includes(humanChoice)) {
+    alert("that is not a weapon !!");
+    return getHumanChoice();
+  }
+  return humanChoice;
 }
-getHumanChoice();
+let human = getHumanChoice();
 
-
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log(
+      `that is a tai !! you choose ${humanChoice} and computer choose ${computerChoice}`
+    );
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(
+      `you win!!  you choose ${humanChoice} and computer choose ${computerChoice}`
+    );
+  } else {
+    console.log(
+      `you lose ! computer chose ${computerChoice} and you choose ${humanChoice}`
+    );
+  }
+}
+playRound(human, computer);
